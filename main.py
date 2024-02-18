43cc3b66f12f716d32eb00bfb01a7049c9dccc15
Richard <richardmrb95@gmail.com>
2024-02-18 22:48:58 +0530

    Basic llm to give pet name suggestions

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 from langchain.llms import OpenAI
-from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
 load_dotenv()
 
-def generate_pet_name(animal_type):
+def generate_pet_name():
     llm = OpenAI(temperature=0.6)  # temperature: 0 --> very safe answer, 1 --> creative but risky answer 
-    prompt_template = PromptTemplate(
-        input_variables=['animal_type'],
-        template = "I have a {animal_type} pet and I want a cool name for it. Suggest me five names for my pet. I is white in color."        
-    )
-    name = 
+    name = llm("I have a cat pet and I want a cool name for it. Suggest me five names for my pet. I is white in color.")
     return name
 if __name__ == '__main__':
     print(generate_pet_name())
