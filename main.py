fd6ee49cec7cd84ee4fc4386ead3d49165de2f11
Richard <richardmrb95@gmail.com>
2024-02-18 23:03:57 +0530

    Updated depricated class with new class and implemented PromptTemplate and LLMChain

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from langchain.llms import OpenAI
+from langchain_openai import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 from dotenv import load_dotenv
 
 load_dotenv()
 
-def generate_pet_name():
-    llm = OpenAI(temperature=0.6)  # temperature: 0 --> very safe answer, 1 --> creative but risky answer 
-    name = llm("I have a cat pet and I want a cool name for it. Suggest me five names for my pet. I is white in color.")
-    return name
+def generate_pet_name(animal_type):
+    llm = OpenAI(temperature=0.7)  # temperature: 0 --> very safe answer, 1 --> creative but risky answer 
+    prompt_template = PromptTemplate(
+        input_variables=['animal_type'],
+        template="I have a {animal_type} pet and I want a cool name for it. Suggest me five names for my pet."
+    )
+    name_chain = LLMChain(llm=llm, prompt=prompt_template)
+    response=name_chain({'animal_type':animal_type})
+    return response
 if __name__ == '__main__':
-    print(generate_pet_name())
+    print(generate_pet_name("cow"))
     
