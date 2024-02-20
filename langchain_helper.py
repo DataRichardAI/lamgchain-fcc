f86f50be846e56ecbb92035806e12faee4bce7d5
Richard <richardmrb95@gmail.com>
2024-02-20 06:40:43 +0530

    Added function langchain_agents in langchain_helper.py to demonstrate use of langchain agents

--- langchain_helper.py
+++ langchain_helper.py
@@ -1,6 +1,9 @@
 from langchain_openai import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
+from langchain.agents import load_tools
+from langchain.agents.initialize import initialize_agent
+from langchain.agents import AgentType
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -14,6 +17,17 @@ def generate_pet_name(animal_type,pet_color):
     name_chain = LLMChain(llm=llm, prompt=prompt_template,output_key="pet_name")
     response=name_chain({'animal_type':animal_type, 'pet_color': pet_color})
     return response
+
+def langchain_agents():
+    llm = OpenAI(temperature=0.5)
+    tools = load_tools(["wikipedia", "llm-math"],llm=llm)
+    agent = initialize_agent(
+        tools,llm,agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+    )
+    result = agent.run("What is the average age of a dog? Multipy the age by 3")
+    print(result)
+    
 if __name__ == '__main__':
-    print(generate_pet_name("cow","red"))
+    langchain_agents()
+    # print(generate_pet_name("cow","red"))
     
